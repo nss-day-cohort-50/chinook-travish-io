80cde6106ff4e9a5c4275e27a9806478b192ba2c
Travis Hall <t.hall89d@gmail.com>
2021-11-19 11:07:58 -0500

    2. brazil customers

---
+++ non_usa_customers.sql
@@ -0,0 +1,8 @@
+SELECT CustomerId, FirstName, LastName, Country
+FROM Customer
+WHERE Country != 'USA';
+
+
+SELECT CustomerId, FirstName, LastName, Country
+FROM Customer
+WHERE Country = 'Brazil';
