2ce76192977fb892a61a225a7f3b04398d24806f
Travis Hall <t.hall89d@gmail.com>
2021-11-19 11:19:24 -0500

    3. brazil_customers_invoices

--- non_usa_customers.sql
+++ non_usa_customers.sql
@@ -6,3 +6,8 @@ WHERE Country != 'USA';
 SELECT CustomerId, FirstName, LastName, Country
 FROM Customer
 WHERE Country = 'Brazil';
+
+SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
+FROM Customer as c
+JOIN Invoice as i ON i.CustomerId = c.CustomerId
+WHERE c.Country = 'Brazil';
